754ff2017d3fda404909d59ce9d8a04294d37297
Anatoliy Bazko <abazko@codenvy.com>
2014-08-14 17:32:02 +0000

    CDEC-9: CLI

---
+++ installation-manager-commons/src/main/java/com/codenvy/cdec/artifacts/ArtifactFactory.java
@@ -0,0 +1,40 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2014] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.cdec.artifacts;
+
+import com.codenvy.cdec.utils.BasedInjector;
+
+/**
+ * @author Anatoliy Bazko
+ */
+public class ArtifactFactory {
+
+    /**
+     * Artifact factory.
+     */
+    public static Artifact createArtifact(String name) {
+        switch (name) {
+            case CDECArtifact.NAME:
+                return BasedInjector.getInstance().getInstance(CDECArtifact.class);
+            case InstallManagerArtifact.NAME:
+                return BasedInjector.getInstance().getInstance(InstallManagerArtifact.class);
+        }
+
+        throw new IllegalArgumentException("Artifact " + name + " not found");
+    }
+}
