99576b52d2d7cfbd89410efe5e4f5441fb264e3f
Anatoliy Bazko <abazko@codenvy.com>
2014-12-15 17:51:18 +0200

    CDEC-22: Implement commands to install CDEC on single node with puppet master

--- installation-manager-core/src/test/java/com/codenvy/im/request/TestRequest.java
+++ installation-manager-core/src/test/java/com/codenvy/im/request/TestRequest.java
@@ -28,6 +28,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /** @author Dmytro Nochevnov */
 public class TestRequest {
@@ -62,5 +63,21 @@ public class TestRequest {
                 .setVersion("00.1.1");
 
         testRequest.getVersion();
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        Request request1 = new Request()
+                .setArtifactName("cdec")
+                .setVersion("1.0.1")
+                .setUserCredentials(new UserCredentials("test token", "test account id"));
+
+        Request request2 = new Request()
+                .setArtifactName("cdec")
+                .setVersion("1.0.1")
+                .setUserCredentials(new UserCredentials("test token", "test account id"));
+
+        assertTrue(request1.equals(request2));
+        assertEquals(request1.hashCode(), request2.hashCode());
     }
 }
