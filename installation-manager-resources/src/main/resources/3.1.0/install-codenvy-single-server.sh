c2fdbbb1d8b39a9c00e38a1bb969598395e6ef94
Anatoliy Bazko <abazko@codenvy.com>
2015-01-09 13:00:31 +0200

    CDEC-96: Automate configuration of DNS entries for on prem installer

--- installation-manager-resources/src/main/resources/3.1.0/install-codenvy-single-server.sh
+++ installation-manager-resources/src/main/resources/3.1.0/install-codenvy-single-server.sh
@@ -7,41 +7,9 @@ VERSION=$1
 CONFIG="codenvy-single-server.properties"
 DIR="${HOME}/codenvy-im"
 
-installJava() {
-    printPrompt; echo "Installing java"
-    wget -q --no-cookies --no-check-certificate --header 'Cookie: oraclelicense=accept-securebackup-cookie' 'http://download.oracle.com/otn-pub/java/jdk/7u17-b02/jre-7u17-linux-x64.tar.gz' --output-document=jre.tar.gz
-
-    tar -xf jre.tar.gz -C ${DIR}
-    mv ${DIR}/jre1.7.0_17 ${DIR}/jre
-
-    rm jre.tar.gz
-}
-
-# $1 - command name
-installOnDebian() {
-    sudo apt-get install $1 -y -q
-}
-
-# $1 - command name
-installOnRedhat() {
-    sudo yum install $1 -y -q
-}
-
-# $1 - command name
-installOnOpensuse() {
-    sudo -s zypper install $1 -y -q
-}
-
-detectOS() {
-    if [ -f /etc/debian_version ]; then
-        OS="Debian"
-        OS_PRINTABLE="Debian"
-    elif [ -f /etc/redhat-release ]; then
-        OS="Redhat"
-        OS_PRINTABLE="Red Hat"
-    elif [ -f /etc/os-release ]; then
-        OS="Opensuse"
-        OS_PRINTABLE="Open Suse"
+checkOS() {
+    if [ -f /etc/redhat-release ]; then
+        OS="Red Hat"
     else
         printPrompt; echo "Operation system isn't supported."
         exit
@@ -52,10 +20,19 @@ detectOS() {
 installPackageIfNeed() {
     command -v $1 >/dev/null 2>&1 || { # check if requered command had already installed earlier
         printPrompt; echo "Installing $1 "
-        installOn${OS} $1
+        sudo yum install $1 -y -q
     }
 }
 
+installJava() {
+    printPrompt; echo "Installing java"
+    wget -q --no-cookies --no-check-certificate --header 'Cookie: oraclelicense=accept-securebackup-cookie' 'http://download.oracle.com/otn-pub/java/jdk/7u17-b02/jre-7u17-linux-x64.tar.gz' --output-document=jre.tar.gz
+
+    tar -xf jre.tar.gz -C ${DIR}
+    mv ${DIR}/jre1.7.0_17 ${DIR}/jre
+
+    rm jre.tar.gz
+}
 
 installIm() {
     printPrompt; echo "Downloading Installation Manager"
@@ -131,7 +108,7 @@ executeCliCommand() {
     [ ${RETVAL} -ne 0 ] && exit ${RETVAL}
 }
 
-executeWithSudoCliCommand() {
+executeWithIMCommand() {
     if [ ! -z "$1" ]; then printPrompt; echo "$1"; fi
     ${DIR}/codenvy-cli/bin/codenvy $2 $3 $4 $5 $6 $7 $8
 
@@ -140,7 +117,7 @@ executeWithSudoCliCommand() {
 }
 
 printPreInstallInfo() {
-    detectOS
+    checkOS
 
     availableRAM=`sudo cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
     availableRAM=$(perl -E "say sprintf('%.1f',${availableRAM}/1024/1024)")
@@ -159,7 +136,7 @@ printPreInstallInfo() {
     printPrompt; echo "5. Install Codenvy by installing Puppet and configuring system parameters"
     printPrompt; echo "6. Boot Codenvy"
     printPrompt; echo
-    printPrompt; echo "We have detected that this node is a ${OS_PRINTABLE} distribution."
+    printPrompt; echo "We have detected that this node is a ${OS} distribution."
     printPrompt; echo
     printPrompt; echo "Configuration : Minimum : Available"
     printPrompt; echo "RAM           : 8GB     : ${availableRAM}GB"
@@ -187,7 +164,8 @@ printPostInstallInfo() {
     printPrompt; echo "Codenvy is ready at http://"${HOSTNAME}"/"
     printPrompt; echo
     printPrompt; echo "Troubleshoot Installation Problems: http://docs.codenvy.com/onpremises/installation-single-node-for-teams/"
-#    printPrompt; echo "Upgrade & Configuration Docs:"
+    printPrompt; echo "Upgrade & Configuration Docs:"
+    # TODO
 }
 
 doInstallStep1() {
@@ -236,14 +214,14 @@ doInstallStep4() {
 doInstallStep5() {
     printPrompt; echo
     printPrompt; echo "BEGINNING STEP 5: INSTALL CODENVY BY INSTALLING PUPPET AND CONFIGURING SYSTEM PARAMETERS"
-    executeWithSudoCliCommand "Installing the latest Codenvy version. Watch progress in /var/log/messages" im-install --step 1-8 --config ${CONFIG} ${ARTIFACT} ${VERSION}
+    executeWithIMCommand "Installing the latest Codenvy version. Watch progress in /var/log/messages" im-install --step 1-8 --config ${CONFIG} ${ARTIFACT} ${VERSION}
     printPrompt; echo "COMPLETED STEP 5: INSTALL CODENVY BY INSTALLING PUPPET AND CONFIGURING SYSTEM PARAMETERS"
 }
 
 doInstallStep6() {
     printPrompt; echo
     printPrompt; echo "BEGINNING STEP 6: BOOT CODENVY"
-    executeWithSudoCliCommand "" im-install --step 9 --config ${CONFIG} ${ARTIFACT} ${VERSION}
+    executeWithIMCommand "" im-install --step 9 --config ${CONFIG} ${ARTIFACT} ${VERSION}
     printPrompt; echo "COMPLETED STEP 6: BOOT CODENVY"
 }
 
