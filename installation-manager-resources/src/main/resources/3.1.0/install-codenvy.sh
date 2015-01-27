c8f265556e040a03be70f24448a75c06f0184913
Anatoliy Bazko <abazko@codenvy.com>
2015-01-27 10:58:14 +0200

    CDEC-102: Ensure working installation script with CentOS 7

--- installation-manager-resources/src/main/resources/3.1.0/install-codenvy.sh
+++ installation-manager-resources/src/main/resources/3.1.0/install-codenvy.sh
@@ -21,9 +21,6 @@ checkOS() {
     fi
     OS_VERSION=`cat /etc/redhat-release | sed 's/.* \([0-9.]*\) .*/\1/' | cut -f1 -d '.'`
 
-    echo "OS:  ${OS_VERSION}"
-    echo "ART: ${VERSION}"
-
     if [ "${VERSION}" == "3.1.0" ] && [ "${OS_VERSION}" -eq "6" ]; then
         echo "Codenvy 3.1.0 can be installed onto CentOS 6.x only"
         exit 1
