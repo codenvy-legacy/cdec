c19d7f444b486b100466b15beb51fea4189c18e0
Anatoliy Bazko <abazko@codenvy.com>
2015-07-06 17:15:08 +0300

    Use Tomcat8

--- update-server-packaging-tomcat/src/main/resources/server/bin/setenv.sh
+++ update-server-packaging-tomcat/src/main/resources/server/bin/setenv.sh
@@ -15,32 +15,33 @@
 # from Codenvy S.A..
 #
 
-# Environment Variable Prerequisites
-
 if [ -z "${CODENVY_LOCAL_CONF_DIR}" ]; then
     echo "Need to set CODENVY_LOCAL_CONF_DIR"
     exit 1
 fi
 
+#Global JAVA options
 [ -z "${JAVA_OPTS}" ]  && JAVA_OPTS="-Xms128m -Xmx512M -XX:MaxPermSize=128m -XX:+UseCompressedOops"
 
-GENERAL_OPTS="-Dcodenvy.local.conf.dir=${CODENVY_LOCAL_CONF_DIR} \
-              -Dcodenvy.logback.smtp.appender=${CODENVY_LOCAL_CONF_DIR}/logback-smtp-appender.xml \
-              -Dcodenvy.syslog.appender=${CODENVY_LOCAL_CONF_DIR}/syslog-appender.xml \
-              -Dcodenvy.logs.dir=${CATALINA_HOME}/logs"
+#Global LOGS DIR
+[ -z "${CODENVY_LOGS_DIR}" ]  && CODENVY_LOGS_DIR="$CATALINA_HOME/logs"
 
-#JMX_OPTS="-Dcom.sun.management.jmxremote.authenticate=true \
-#          -Dcom.sun.management.jmxremote.password.file=${CATALINA_HOME}/conf/jmxremote.password \
-#          -Dcom.sun.management.jmxremote.access.file=${CATALINA_HOME}/conf/jmxremote.access \
-#          -Dcom.sun.management.jmxremote.ssl=false"
+[ -z "${JPDA_ADDRESS}" ]  && JPDA_ADDRESS="8000"
 
-#REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-REMOTE_DEBUG=
+#Tomcat options
+[ -z "${CATALINA_OPTS}" ]  && CATALINA_OPTS="-Dcom.sun.management.jmxremote  \
+                                             -Dcom.sun.management.jmxremote.ssl=false \
+                                             -Dcom.sun.management.jmxremote.authenticate=false \
+                                             -Dcodenvy.local.conf.dir=${CODENVY_LOCAL_CONF_DIR} \
+                                             -Dcodenvy.logback.smtp.appender=${CODENVY_LOCAL_CONF_DIR}/logback-smtp-appender.xml \
+                                             -Dcodenvy.syslog.appender=${CODENVY_LOCAL_CONF_DIR}/syslog-appender.xml"
 
-export CATALINA_HOME
-export CATALINA_TMPDIR
-export JAVA_OPTS="$JAVA_OPTS $GENERAL_OPTS $JMX_OPTS $REMOTE_DEBUG $JMX_OPTS"
-export CLASSPATH="${CATALINA_HOME}/conf/:${JAVA_HOME}/lib/tools.jar"
+#Class path
+[ -z "${CLASSPATH}" ]  && CLASSPATH="${CATALINA_HOME}/conf/:${JAVA_HOME}/lib/tools.jar"
+
+export JAVA_OPTS="$JAVA_OPTS  -Dcodenvy.logs.dir=${CODENVY_LOGS_DIR}"
 
 echo "Using LOCAL_CONF_DIR:  $CODENVY_LOCAL_CONF_DIR"
+
+
 
