84dc8acc1a611a3e4841cb10200e06ed9bb16644
Anatoliy Bazko <abazko@codenvy.com>
2015-07-09 16:03:14 +0300

    Fix detecting HOST_NAME when install script was running in silent mode

--- installation-manager-resources/src/main/resources/im-scripts/install-codenvy.sh
+++ installation-manager-resources/src/main/resources/im-scripts/install-codenvy.sh
@@ -157,7 +157,7 @@ insertProperty() {
 }
 
 updateHostsFile() {
-    [ -z ${HOST_NAME} ] && HOST_NAME=`grep host[_url]*=.* ${CONFIG} | cut -f2 -d '='`
+    [ -z ${HOST_NAME} ] && HOST_NAME=$(grep host_url\\W*=\\W*.* ${CONFIG} | sed 's/host_url\W*=\W*\(.*\)/\1/')
 
     if ! sudo grep -Eq "127.0.0.1.*puppet" /etc/hosts; then
         echo '127.0.0.1 puppet' | sudo tee --append /etc/hosts > /dev/null
