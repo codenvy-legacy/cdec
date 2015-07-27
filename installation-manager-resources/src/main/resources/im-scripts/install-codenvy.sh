b78dcb529670f074b36645e35ae57a2ec1498fa6
Anatoliy Bazko <abazko@codenvy.com>
2015-07-27 16:57:25 +0300

    CDEC-296: Install Codenvy from the custom folder

--- installation-manager-resources/src/main/resources/im-scripts/install-codenvy.sh
+++ installation-manager-resources/src/main/resources/im-scripts/install-codenvy.sh
@@ -405,10 +405,10 @@ doInstallCodenvy() {
         fi
 
         if [ ${CODENVY_TYPE} == "multi" ]; then
-            executeIMCommand im-install --step ${STEP}-${STEP} --force --multi --config ${CONFIG} ${ARTIFACT} ${VERSION} >> install.log
+            executeIMCommand im-install --step ${STEP}-${STEP} --forceInstall --multi --config ${CONFIG} ${ARTIFACT} ${VERSION} >> install.log
             validateExitCode $?
         else
-            executeIMCommand im-install --step ${STEP}-${STEP} --force --config ${CONFIG} ${ARTIFACT} ${VERSION} >> install.log
+            executeIMCommand im-install --step ${STEP}-${STEP} --forceInstall --config ${CONFIG} ${ARTIFACT} ${VERSION} >> install.log
             validateExitCode $?
         fi
     done
